4d103ada78b8b1e31dc14f3bdecf622df2a11698
Nathan King <nathanking@Nathans-MacBook-Air.local>
2026-01-27 13:20:44 -0500

    Started the check stability function,to be completed

--- check_validity.py
+++ check_validity.py
@@ -6,21 +6,57 @@ def check_validity(n, matching):
     
     students = set()
     hospitals = set()
-    for a,b in matching:
-        if a<1 or a>n:
-            return (False, f"INVALID (Hospital {a} is out of range)")
+    for h,s in matching:
+        if h<1 or s>n:
+            return (False, f"INVALID (Hospital {h} is out of range)")
         
-        if b<1 or b>n:
-            return (False, f"INVALID (Student {b} is out of range)")
+        if s<1 or s>n:
+            return (False, f"INVALID (Student {s} is out of range)")
         
         
-        if a in hospitals:
-            return (False, f"INVALID (Hospital {a} is matched twice)")
-        if b in students:
-            return ((False, f"INVALID (Student {b} is matched twice)"))
+        if h in hospitals:
+            return (False, f"INVALID (Hospital {h} is matched twice)")
+        if s in students:
+            return ((False, f"INVALID (Student {s} is matched twice)"))
             
-        students.add(b)
-        hospitals.add(a)
+        students.add(s)
+        hospitals.add(h)
     return (True, "VALID")
-        
 
+#now check stability 
+
+def check_stability(n, matchings, hospital_prefs, student_prefs):
+    #matching is list of tuples
+    #hospital prefs is dictionary of hospital: preference list
+    #stucent prefs is vice versa
+
+    bl, msg = check_validity(n, matchings)    
+    if bl == False:
+        return False, msg
+
+    match_h = {}
+    match_s = {}
+   
+    for h, s in matchings:
+        match_h[h] = s
+        match_s[s] = h
+
+        """This allows lookups in O(1) time, which is better than iterating through the list in every iteration"""
+
+    """
+    I will do this algorithm later
+
+
+    Iterate over hospitals
+    which ever the hospital is matched to, check students to the left.
+    check students to the left, do they prefer this hospital over their current? 
+
+    If yes, Return False for unstable match,
+    if not, continue
+
+    once all hospitals have been iterated over, and if there is no unstable match, 
+    then the matching is stable, and return True.
+
+    """
+
+    return (True, "VALID STABLE")
